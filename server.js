bda144090ee6c3c2160742779aa2b53ecdb8dca6
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-04-25 16:37:29 -0300

    post livros

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
-import http from 'http';
+// import http from 'http';
+import app from './src/app.js';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const rotas = {
-    '/': 'Hello, World!',
-    '/livros': 'Entrei na rota livros',
-    '/autores': 'Entrei na rota autores',
-    '/sobre': 'Entrei na rota sobre',
-}
+// const server = http.createServer((req, res) => {
+//   res.writeHead(200, { 'Content-Type': 'text/plain' });
+//   res.end(rotas[req.url]);
+// });
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end(rotas[req.url]);
-});
+// server.listen(PORT, () => {
+//   console.log('Server running at http://localhost:3000/');
+// });
 
-server.listen(PORT, () => {
-  console.log('Server running at http://localhost:3000/');
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}/`);
 });
---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+const app = express();
+app.use(express.json());
+
+const livros = [
+    {
+        id: 1,
+        título: "O Senhor dos Anéis"
+    },
+    {
+        id: 2,
+        título: "O Hobbit"
+    }
+];
+
+app.get("/", (req, res) => {
+    res.status(200).send("Curso de Noje.js");
+});
+
+app.get("/livros", (req, res) => {
+    res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+    livros.push(req.body);
+    res.status(201).send("Livro cadastrado com sucesso");
+});
+
+export default app;
