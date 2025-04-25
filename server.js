2f2f28a801f325544fc74f5617b6c3bcfdf8da72
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-04-25 16:06:14 -0300

    rotas

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 import http from 'http';
 
+const PORT = 3000;
+
+const rotas = {
+    '/': 'Hello, World!',
+    '/livros': 'Entrei na rota livros',
+    '/autores': 'Entrei na rota autores',
+    '/sobre': 'Entrei na rota sobre',
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello, World!\n');
+  res.end(rotas[req.url]);
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log('Server running at http://localhost:3000/');
 });
