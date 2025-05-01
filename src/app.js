c7e782fc308d6eb50a1ffef11670bfc923d1e855
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-01 11:19:26 -0300

    config env

--- src/app.js
+++ src/app.js
@@ -1,10 +1,7 @@
 import express from 'express';
-import dotenv from 'dotenv';
 
 import connectDB from './config/dbconnect.js';
 import livro from './models/Livro.js';
-
-dotenv.config();
 
 const conexao = await connectDB();
 
