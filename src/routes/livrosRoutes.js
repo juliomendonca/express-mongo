91b8b8d25f16e1c719a7aa6da92ed8d360b4d13f
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-01 13:56:55 -0300

    controllers refatorado

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -3,24 +3,14 @@ import LivroController from "../controllers/livroController.js";
 
 const routes = express.Router();
 
-routes.get("/livros", (req, res) => {
-    LivroController.listarLivros(req, res);
-});
+routes.get("/livros", LivroController.listarLivros);
 
-routes.get("/livros/:id", (req, res) => {
-    LivroController.listarLivroPorId(req, res);
-});
+routes.get("/livros/:id", LivroController.listarLivroPorId);
 
-routes.post("/livros", (req, res) => {
-    LivroController.cadastrarLivro(req, res);
-});
+routes.post("/livros", LivroController.cadastrarLivro);
 
-routes.put("/livros/:id", (req, res) => {
-    LivroController.atualizarLivro(req, res);
-});
+routes.put("/livros/:id", LivroController.atualizarLivro);
 
-routes.delete("/livros/:id", (req, res) => {
-    LivroController.excluirLivro(req, res);
-});
+routes.delete("/livros/:id", LivroController.excluirLivro);
 
 export default routes;
