f0f97344dfe13ff8c9831cffc582f20a0e97296f
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-01 09:54:24 -0300

    novos métodos

--- src/app.js
+++ src/app.js
@@ -6,13 +6,19 @@ app.use(express.json());
 const livros = [
     {
         id: 1,
-        título: "O Senhor dos Anéis"
+        titulo: "O Senhor dos Anéis"
     },
     {
         id: 2,
-        título: "O Hobbit"
+        titulo: "O Hobbit"
     }
 ];
+
+function buscaLivro(id) {
+    return livros.findIndex(livro => {
+        return livro.id === Number(id);
+    });
+}
 
 app.get("/", (req, res) => {
     res.status(200).send("Curso de Noje.js");
@@ -22,9 +28,35 @@ app.get("/livros", (req, res) => {
     res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    if (index === -1)
+        return res.status(404).send("Livro não encontrado");
+
+    res.status(200).json(livros[index]);
+});
+
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("Livro cadastrado com sucesso");
+});
+
+app.put("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    if (index === -1)
+        return res.status(404).send("Livro não encontrado");
+
+    livros[index].titulo = req.body.titulo;
+    res.status(200).send("Livro atualizado com sucesso");
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    if (index === -1)
+        return res.status(404).send("Livro não encontrado");
+
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso");
 });
 
 export default app;
