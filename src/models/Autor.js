148ef438f8dbe463eafc3e4a18a2a34f7de2c940
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-01 14:19:40 -0300

    funcionalidades autores

---
+++ src/models/Autor.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const autorSchema = new mongoose.Schema({
+    id: { type: mongoose.Schema.Types.ObjectId, auto: true },
+    nome: { type: String, required: true },
+    nacionalidade: { type: String, required: true }
+},
+    {
+        versionKey: false
+    }
+);
+
+const autor = mongoose.model("autores", autorSchema);
+
+export {
+    autorSchema,
+    autor
+}
+
--- src/models/Livro.js
+++ src/models/Livro.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
+import { autorSchema } from "./Autor.js";
 
 const livroSchema = new mongoose.Schema({
     id: { type: mongoose.Schema.Types.ObjectId, required: true },
-    titulo: { type: mongoose.Schema.Types.String, required: true },
-    editora: { type: mongoose.Schema.Types.String, required: true },
+    titulo: { type: String, required: true },
+    editora: { type: String, required: true },
     preco: { type: mongoose.Schema.Types.Double, required: true },
-    paginas: { type: mongoose.Schema.Types.Int32, required: true }
+    paginas: { type: mongoose.Schema.Types.Int32, required: true },
+    autor: autorSchema,
 }, {
     versionKey: false
 });
