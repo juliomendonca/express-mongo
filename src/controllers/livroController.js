5744faa997f8564243907f6fa5ad3ce6357604fe
Júlio Cézar Mendonça <juliomendonca@outlook.com>
2025-05-01 14:28:06 -0300

    controllers ok

--- src/controllers/livroController.js
+++ src/controllers/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js";
 
 class LivroController {
     static async listarLivros(req, res) {
@@ -21,9 +22,20 @@ class LivroController {
     };
 
     static async cadastrarLivro(req, res) {
+        const novoLivro = req.body;
+
         try {
-            const novoLivro = await livro.create(req.body);
-            res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
+            const autorEncontrado = await autor.findById(novoLivro.autor);
+
+            if (!autorEncontrado) {
+                return res.status(404).json({ message: "Autor não encontrado" });
+            }
+
+            const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+
+            const livroCriado = await livro.create(livroCompleto);
+
+            res.status(201).json({ message: "criado com sucesso", livro: livroCriado });
         } catch (erro) {
             res.status(500).json({ message: `${erro.message} - falha ao cadastrar livro` });
         }
